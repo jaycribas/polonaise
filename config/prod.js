4cb5a9864beb8d7b4ab6612c4948337a7a342691
ontoneio <ontoneio.mm@gmail.com>
2018-04-04 15:15:11 -0700

    Add spotify configuration keys

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,4 @@
 module.exports = {
- oauthPublicKey: process.env.OAUTHIO_PUBLIC_KEY,
- oauthSecretKey: process.env.OAUTHIO_SECRET_KEY,
- oauthRedirect: 'https://polonaise-mv.herokuapp.com/oauth/redirect'
+  spotifyClientId: process.env.SPOTIFY_CLIENT_ID,
+  spotifySecretId: process.env.SPOTIFY_SECRET_ID
 }
