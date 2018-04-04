d85f97e0ef9e81f1767eee0d0b0038f37c3409e8
ontoneio <ontoneio.mm@gmail.com>
2018-04-03 19:14:31 -0700

    Add redirect URL for local and production authentication

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,5 @@
 module.exports = {
  oauthPublicKey: process.env.OAUTHIO_PUBLIC_KEY,
- oauthSecretKey: process.env.OAUTHIO_SECRET_KEY
+ oauthSecretKey: process.env.OAUTHIO_SECRET_KEY,
+ oauthRedirect: 'https://polonaise-mv.herokuapp.com/oauth/redirect'
 }
