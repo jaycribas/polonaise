4eda3b32fe230c31aaaeedd03d6336eee9919941
Jay Cribas <feverjay@gmail.com>
2018-04-05 14:11:01 -0700

    Adds client public folder to feathers server, now reads client css file

--- src/app.js
+++ src/app.js
@@ -29,6 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 // app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
 // Host the public folder
 app.use('/', express.static(app.get('public')));
+app.use('/', express.static(__dirname + '/client/public'));
 
 // Set up Plugins and providers
 app.configure(express.rest());
